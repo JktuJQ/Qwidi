ee601f36c066bfbe79032e914231692b1e278531
KageSHDS <RabITquestion@yandex.ru>
2024-11-16 23:07:34 +0300

    wrote structure, navigation, routing, store

---
+++ frontend/src/utils/const.jsx
@@ -0,0 +1,6 @@
+export const Admin_route = "/admin";
+export const Login_route = "/login";
+export const Registration_route = "/registration";
+export const Shop_route = "/";
+export const Basket_route = "/basket";
+export const Clothes_route = "/clothes";
