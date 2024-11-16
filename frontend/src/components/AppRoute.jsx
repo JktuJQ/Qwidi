cb2cc1915f8a592727f4998f1da7fabc1fa3dd1a
KageSHDS <RabITquestion@yandex.ru>
2024-11-16 17:55:30 +0300

    configured ESLint and Prettier

---
+++ frontend/src/components/AppRoute.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const AppRoute = () => {
+  return <div>AppRoute</div>;
+};
+
+export default AppRoute;
---
+++ frontend/src/pages/ClothesCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ClothesCard = () => {
+  return <div>ClothesCard</div>;
+};
+
+export default ClothesCard;
